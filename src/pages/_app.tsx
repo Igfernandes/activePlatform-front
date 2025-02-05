7c60c6e564f84700731fb3a934bfbb1afe4cf631
igfernandes <fernandes7.1@outlook.com>
2025-02-05 05:47:14 -0300

    chore: Adding declaration of fonts in project in the file _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from "next/app";
 import "../styles/globals.css";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { Footer } from "@components/Footer";
+import { poppins } from "@assets/fonts/poppins";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,7 +16,10 @@ const queryClient = new QueryClient({
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <div className={poppins.className}>
+        <Component {...pageProps} />
+        <Footer />
+      </div>
     </QueryClientProvider>
   );
 }
