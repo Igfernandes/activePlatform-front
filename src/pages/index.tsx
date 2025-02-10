874352cbf5dc5d8d9ee27981cfd96f72343641e0
igfernandes <fernandes7.1@outlook.com>
2025-02-09 21:19:20 -0300

    feat: created page forgot-password

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import { UserShare } from "@assets/Icons/colorful/UserShare";
-import { LoginForm } from "@components/Login/form";
+import { LoginForm } from "@components/Login/Form";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import { useRememberMe } from "@hooks/useRememberMe";
 import { useEffect } from "react";
 import usePostRememberMe from "../services/Authentications/RememberMe/usePostRememberMe";
 import { PostRememberMePayload } from "../services/Authentications/RememberMe/type";
+import { ExternalContainer } from "@components/shared/layouts/ExternalContainer";
 
 export default function Home() {
   const { t } = useTranslation("login");
@@ -24,26 +25,24 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="bg-[url(/imgs/backgrounds/3d-wave.png)] bg-cover bg-no-repeat h-[90vh] flex items-center font-poppins">
-      <div className="container w-[424px] mx-auto shadow-sm border-secondary border-2 px-6 py-10 rounded-[1.5rem]">
-        <div className="row">
-          <div className="column text-center">
-            <div className="my-6">
-              <UserShare className="mx-auto" />
-            </div>
-            <div className="mb-1">
-              <h2 className="text-2xl">
-                <strong>{t("welcome")}</strong>
-              </h2>
-            </div>
-            <div className="mb-6">
-              <p className="text-sm">{t("text_presentation")}</p>
-            </div>
-            <LoginForm />
+    <ExternalContainer>
+      <div className="row">
+        <div className="column text-center">
+          <div className="my-6">
+            <UserShare className="mx-auto" />
           </div>
+          <div className="mb-1">
+            <h2 className="text-2xl">
+              <strong>{t("welcome")}</strong>
+            </h2>
+          </div>
+          <div className="mb-6">
+            <p className="text-sm">{t("text_presentation")}</p>
+          </div>
+          <LoginForm />
         </div>
       </div>
-    </main>
+    </ExternalContainer>
   );
 }
 
