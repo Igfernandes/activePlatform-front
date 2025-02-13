890c77a6c7f2902dec87a183e59a1a4cb96f6cec
igfernandes <fernandes7.1@outlook.com>
2025-02-13 18:10:57 -0300

    feat: created new directory to vaiables of system

---
+++ src/configs/envs/index.ts
@@ -0,0 +1,11 @@
+import { EnvironmentProprieties, EnvironmentTypes } from "./type";
+
+export const LANGUAGE_I18N = process.env.NEXT_PUBLIC_LANGUAGE;
+export const env: EnvironmentProprieties = {
+  PROD: {
+    API_URL: process.env.NEXT_PUBLIC_PROD_URL,
+  },
+  DEV: {
+    API_URL: process.env.NEXT_PUBLIC_DEV_URL,
+  },
+}[process.env.NEXT_PUBLIC_ENVIRONMENT as EnvironmentTypes];
