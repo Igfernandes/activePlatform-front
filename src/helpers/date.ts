c7628ddf7fdca6dc5db0b6f25551b86bd688a9ed
igfernandes <fernandes7.1@outlook.com>
2025-03-13 20:09:36 -0300

    feat: Created handle to date mask

---
+++ src/helpers/date.ts
@@ -0,0 +1,20 @@
+export const handleMaskDate = (event: React.ChangeEvent<HTMLInputElement>) => {
+  let value = event.target.value.replace(/\D/g, ""); // Remove tudo que não for número
+
+  if (value.length > 2) {
+    value = value.slice(0, 2) + "/" + value.slice(2);
+  }
+  if (value.length > 5) {
+    value = value.slice(0, 5) + "/" + value.slice(5, 9);
+  }
+
+  event.target.value = value;
+};
+
+export function isMonthValid(month: number) {
+  return month > 0 && month <= 12;
+}
+
+export function isDayValid(day: number) {
+  return day > 0 && day <= 31;
+}
