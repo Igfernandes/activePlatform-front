4124c7ef2c90b5dc93a0ed6943959a1f1c24f8ae
igfernandes <fernandes7.1@outlook.com>
2025-03-26 17:51:24 -0300

    feat: Created page to services

---
+++ src/helpers/routes/index.ts
@@ -0,0 +1,33 @@
+import { SetParamsProps, SetQueriesProps } from "./types";
+
+export function setQueries({ url, query }: SetQueriesProps) {
+  let queryCurrent = "";
+  if (query) {
+    Object.entries(query).forEach(([label, value]) => {
+      if (typeof value != "undefined" || value != null)
+        queryCurrent += `${label}=${value}&`;
+    });
+  }
+
+  return `${url}?${queryCurrent.slice(0, -1)}`;
+}
+
+export function setParams({ url, data }: SetParamsProps) {
+  let urlWithParam = url;
+
+  if (data && Object.keys(data).length > 0) {
+    Object.entries(data).forEach(([label, value]) => {
+      urlWithParam = value
+        ? urlWithParam.replaceAll(`{${label}}`, value)
+        : urlWithParam.replaceAll(`{${label}}`, "");
+    });
+    urlWithParam = urlWithParam.includes("//")
+      ? urlWithParam.replaceAll("//", "/")
+      : urlWithParam;
+  }
+
+  // SE AINDA HOUVER ALGUM PARAMETRO QUE NAO FOI SUBSTITUIDO, REMOVER (REMOVE TUDO DENTRO DAS CHAVES INCLUINDO AS CHAVES)
+  urlWithParam = urlWithParam.replace(/ *\{[^)]*\} */g, "");
+
+  return urlWithParam;
+}
